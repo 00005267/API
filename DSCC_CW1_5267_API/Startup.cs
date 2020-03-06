a807630264b13deefbc2913a088fd88acf1d9c7b
5591 <5591>
2020-03-05 17:09:49 -0800

    changed error

--- DSCC_CW1_5267_API/Startup.cs
+++ DSCC_CW1_5267_API/Startup.cs
@@ -1,21 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using DSCC_CW1_5267_API.DataBaseContext;
 using DSCC_CW1_5267_API.Repositories;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.HttpOverrides;
-using System.Net;
 
 
 namespace DSCC_CW1_5267_API
